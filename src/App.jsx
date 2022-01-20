d04b40fe2b8a96b32532fe614500c2ce767e79e7
Claire Durand <lepretre.c@gmail.com>
2022-01-20 18:49:05 +0100

    Home page created

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {
 import AppProvider from "./contexts/AppProvider";
 import Home from './pages/Home/Home';
 import Products from './pages/Products/Products';
-import Product from './pages/Product/Product';
+import Product from './pages/ProductPage/ProductPage';
 import Payments from './pages/Payments/Payments';
 
 import './App.css';
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,87 @@
 import React from 'react';
+import './Home.css';
+import painting from '../../assets/images/painting.jpg'
+import Shelf from '../../assets/images/Shelf.jpg'
+import WoodenFloor from '../../assets/images/WoodenFloor.jpg'
+import kitchen from '../../assets/images/kitchen.jpg'
+import bathroom from '../../assets/images/kitchen.jpg'
+import carpentry from '../../assets/images/carpentry.jpg'
+import garden from '../../assets/images/garden.jpg'
+// import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <main></main>;
+  return <main>
+    <div className='recipes-all'>
+      <h1>Choose your daily recipes</h1>
+      <div className='daily-recipes'>
+        <div className='recipes'>
+          {/* <Link to=""> */}
+            <img
+              className='recipes-img'
+              src={painting}
+              alt='recipes-img'/>
+            <p>Painting</p>
+          {/* </Link> */}
+        </div>
+        <div className='recipes'>
+          {/* <Link to=""> */}
+            <img
+              className='recipes-img'
+              src={Shelf}
+              alt='recipes-img'/>
+            <p>Shelf</p>
+          {/* </Link> */}
+        </div>
+        <div className='recipes'>
+          {/* <Link to=""> */}
+            <img
+              className='recipes-img'
+              src={WoodenFloor}
+              alt='recipes-img'/>
+            <p>Wooden Floor</p>
+          {/* </Link> */}
+        </div>
+      </div>
+      <div className='productClass'>
+        <div className='product'>
+        {/* <Link to=""> */}
+          <img
+            className='product-img'
+            src={kitchen}
+            alt='kitchen'/>
+          <p>Kitchen</p>
+        {/* </Link> */}
+        </div>
+        <div className='product'>
+        {/* <Link to=""> */}
+          <img
+            className='product-img'
+            src={bathroom}
+            alt='bathroom'/>
+          <p>Bathroom</p>
+        {/* </Link> */}
+        </div>
+        <div className='product'>
+        {/* <Link to=""> */}
+        <img
+            className='product-img'
+            src={carpentry}
+            alt='carpentry'/>
+          <p>Carpentry</p>
+        {/* </Link> */}
+        </div>
+        <div className='product'>
+        {/* <Link to=""> */}
+        <img
+            className='product-img'
+            src={garden}
+            alt='garden'/>
+          <p>Garden</p>
+        {/* </Link> */}   
+        </div>             
+        </div>
+    </div>
+  </main>;
 };
 
 export default Home;
